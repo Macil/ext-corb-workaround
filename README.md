699666384539b535c7f613059361d89be801ce11
Chris Cowan <agentme49@gmail.com>
2020-02-03 15:41:21 -0800

    Initial commit in working state.

---
+++ README.md
@@ -0,0 +1,49 @@
+# ext-corb-workaround
+
+This package is a work-around for a bug with Cross-Origin Request Blocking (CORB) as implemented in Chrome extensions.
+
+According to https://www.chromium.org/Home/chromium-security/extension-content-script-fetches, "content scripts should be subject to the same request rules as the page they are running within", but currently Chrome blocks requests from content scripts if the extension has permissions to the requested domain, regardless of whether the page it's running within also has permissions to the requested domain because of CORS.
+
+In order to work around this issue, this package allows creating a replacement `XMLHttpRequest` object for a content script to use which proxies its connections through the page's context, so the request is treated exactly as a request from the parent page instead of from the extension.
+
+Because this proxies the connection through the web page's javascript context, it is possible for the web page to modify the content of the request. **This package should only be used by extensions that trust the web page the content script is running in.** This makes this package a good fit for an extension that adds features to one specific site. It is not recommended to use this package in a content script that executes inside all web pages.
+
+**If possible, follow the recommendations inside the ["Recommended Developer Actions" section of Chrome's CORB/extension documentation](https://www.chromium.org/Home/chromium-security/extension-content-script-fetches#TOC-Recommended-Developer-Actions) instead of using this package.** This package should only be used where you specifically want the request to run as if it came from the content script's page rather than as the extension.
+
+## API
+
+This library exports two functions:
+
+### getXMLHttpRequest(): typeof XMLHttpRequest
+
+This function returns an XMLHttpRequest-like object which can be used like XMLHttpRequest, but with all of its requests proxied through the page's context.
+
+Usage example:
+
+```js
+import {getXMLHttpRequest} from 'ext-corb-workaround';
+
+function getWithCorbWorkaround(url) {
+  return new Promise((resolve, reject) => {
+    const XMLHttpRequest = getXMLHttpRequest();
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('loadend', () => {
+      if (xhr.status !== 200) {
+        reject(new Error(`Error fetching: ${xhr.statusText}`));
+      } else {
+        resolve(xhr.responseText);
+      }
+    });
+    xhr.open('GET', url);
+    xhr.send();
+  });
+}
+```
+
+### installGlobally(): void
+
+This is a convenience function which replaces `window.XMLHttpRequest` with the value returned by `getXMLHttpRequest()`. This may be useful for an extension content script that wants to globally opt into proxying its connections through the page.
+
+## Types
+
+[TypeScript](https://www.typescriptlang.org/) type definitions for this module are included.
---
+++ src/index.ts
@@ -0,0 +1,258 @@
+import once from 'lodash/once';
+
+export const installGlobally = once((): void => {
+  window.XMLHttpRequest = getXMLHttpRequest();
+});
+
+// TODO add replacement fetch too?
+
+export const getXMLHttpRequest: () => typeof XMLHttpRequest = once(() => {
+  const scr = document.createElement('script');
+  scr.textContent = '(' + inPageWorld + ')();';
+  document.documentElement.appendChild(scr).remove();
+
+  const instancesById: {[id: number]: CORBWorkaroundXMLHttpRequest} = {};
+
+  const channel = new MessageChannel();
+  const port = channel.port1;
+  port.addEventListener('message', event => {
+    const {id, type} = event.data;
+    const xhr = instancesById[id];
+    switch (type) {
+      case 'COMPLETE': {
+        const {headers, readyState, status, response, responseText, responseURL} = event.data;
+        const headersObj: {[header: string]: string} = {};
+        headers.split('\r\n').forEach((line: string) => {
+          const [name, value] = line.split(':');
+          if (name && value) {
+            headersObj[name.toLowerCase().trim()] = value.trim();
+          }
+        });
+        xhr._headers = headers;
+        xhr._headersObj = headersObj;
+        xhr.status = status;
+        xhr.readyState = readyState;
+        xhr.response = response;
+        xhr.responseText = responseText;
+        xhr.responseURL = responseURL;
+        delete instancesById[id];
+
+        for (const event of ['load', 'loadend', 'readystatechange']) {
+          const eventObj = {name: event, target: xhr};
+          const handlers = Object.prototype.hasOwnProperty.call(xhr._eventListeners, event)
+            ? xhr._eventListeners[event]
+            : [];
+          handlers.forEach(handler => handler(eventObj));
+          if ((xhr as any)['on' + event]) {
+            (xhr as any)['on' + event](eventObj);
+          }
+        }
+        break;
+      }
+      default: {
+        // eslint-disable-next-line no-console
+        console.error('ext-corb-workaround: Unknown event in content script:', event);
+      }
+    }
+  });
+  port.addEventListener('messageerror', event => {
+    // eslint-disable-next-line no-console
+    console.error('ext-corb-workaround: Unknown error in content script:', event);
+  });
+  port.start();
+  window.postMessage({type: 'PORT_FOR_CORB_WORKAROUND'}, document.location.origin, [channel.port2]);
+
+  let nextFreeId = 0;
+  class CORBWorkaroundXMLHttpRequest {
+    public static UNSENT = 0;
+    public static OPENED = 1;
+    public static HEADERS_RECEIVED = 2;
+    public static LOADING = 3;
+    public static DONE = 4;
+
+    public UNSENT = 0;
+    public OPENED = 1;
+    public HEADERS_RECEIVED = 2;
+    public LOADING = 3;
+    public DONE = 4;
+
+    private _id = nextFreeId++;
+    public _eventListeners: {[name: string]: Function[]} = {};
+    public _headersObj: {[name: string]: string} = {};
+
+    public status = 0;
+    public readyState = 0;
+    public response = '';
+    public responseText = '';
+    public responseURL = '';
+    public _headers = '';
+
+    public responseType: any;
+    public withCredentials: any;
+
+    public setRequestHeader: any;
+    public open: any;
+    public send: any;
+    public abort: any;
+
+    public constructor() {
+      const id = this._id;
+      instancesById[id] = this;
+      port.postMessage({type: 'NEW_XHR', id});
+      for (const prop of ['responseType', 'withCredentials'] as const) {
+        let value: any = undefined;
+        Object.defineProperty(this, prop, {
+          get() {
+            return value;
+          },
+          set(newValue) {
+            value = newValue;
+            port.postMessage({type: 'SET', id, prop, value: newValue});
+          }
+        });
+      }
+      for (const method of ['setRequestHeader', 'open', 'send', 'abort'] as const) {
+        this[method] = (...args: any[]) => {
+          port.postMessage({type: 'CALL', id, method, args}, transferrables(args));
+        };
+      }
+    }
+    public getAllResponseHeaders(): string {
+      return this._headers;
+    }
+    public getResponseHeader(header: string): string | void {
+      header = header.toLowerCase();
+      if (Object.prototype.hasOwnProperty.call(this._headersObj, header)) {
+        return this._headersObj[header];
+      }
+    }
+    public addEventListener(event: string, handler: Function) {
+      if (!Object.prototype.hasOwnProperty.call(this._eventListeners, event)) {
+        this._eventListeners[event] = [];
+      }
+      this._eventListeners[event].push(handler);
+    }
+    public removeEventListener(event: string, handler: Function) {
+      if (Object.prototype.hasOwnProperty.call(this._eventListeners, event)) {
+        this._eventListeners[event] = this._eventListeners[event].filter(
+          _handler => handler !== _handler
+        );
+      }
+    }
+  }
+  return (CORBWorkaroundXMLHttpRequest as any) as typeof XMLHttpRequest;
+});
+
+function transferrables(list: any[]): any[] {
+  return list
+    .map(value => {
+      if (value && typeof value === 'object' && value.__proto__) {
+        if (value.__proto__.constructor.name === 'ArrayBuffer') {
+          return value;
+        }
+        if (
+          value.__proto__.__proto__ &&
+          value.__proto__.__proto__.constructor.name === 'TypedArray'
+        ) {
+          return value.buffer;
+        }
+      }
+    })
+    .filter(Boolean);
+}
+
+// only gets executed in injected script in page's world
+const inPageWorld = `
+function inPageWorld() {
+  'use strict';
+
+  // save a reference to XHR so we use the original instead of any replacements that extensions may place.
+  const XMLHttpRequest = window.XMLHttpRequest;
+
+  function transferrables(list) {
+    return list
+      .map(value => {
+        if (value && typeof value === 'object' && value.__proto__) {
+          if (value.__proto__.constructor.name === 'ArrayBuffer') {
+            return value;
+          }
+          if (
+            value.__proto__.__proto__ &&
+            value.__proto__.__proto__.constructor.name === 'TypedArray'
+          ) {
+            return value.buffer;
+          }
+        }
+      })
+      .filter(Boolean);
+  }
+
+  function handler(event) {
+    if (!event.data || event.data.type !== 'PORT_FOR_CORB_WORKAROUND') {
+      return;
+    }
+
+    window.removeEventListener('message', handler);
+    const port = event.ports[0];
+    const instancesById = {};
+    port.addEventListener('message', event => {
+      const {id} = event.data;
+      switch (event.data.type) {
+        case 'NEW_XHR': {
+          const xhr = (instancesById[id] = new XMLHttpRequest());
+          xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState !== 4) {
+              return;
+            }
+            delete instancesById[id];
+            let responseText;
+            try {
+              responseText = xhr.responseText;
+            } catch (err) {
+              // ignore
+            }
+            port.postMessage(
+              {
+                type: 'COMPLETE',
+                id,
+                headers: xhr.getAllResponseHeaders(),
+                readyState: xhr.readyState,
+                status: xhr.status,
+                responseURL: xhr.responseURL,
+                response: xhr.response,
+                responseText
+              },
+              transferrables([xhr.response])
+            );
+          });
+          break;
+        }
+        case 'SET': {
+          const {prop, value} = event.data;
+          instancesById[id][prop] = value;
+          break;
+        }
+        case 'CALL': {
+          const {method, args} = event.data;
+          // Let abort calls silently fail if the XHR isn't present.
+          if (method === 'abort' && !instancesById[id]) {
+            break;
+          }
+          instancesById[id][method](...args);
+          break;
+        }
+        default: {
+          // eslint-disable-next-line no-console
+          console.error('ext-corb-workaround: Unknown event in page world:', event);
+        }
+      }
+    });
+    port.addEventListener('messageerror', event => {
+      // eslint-disable-next-line no-console
+      console.error('ext-corb-workaround: Unknown error in page world:', event);
+    });
+    port.start();
+  }
+  window.addEventListener('message', handler);
+}
+`;
